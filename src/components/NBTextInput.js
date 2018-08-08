04b497fa4680e0edb74b39fa68e2b5486a0d480d
Dicky Indra Asmara <dickyindraofficial@gmail.com>
2018-08-08 13:53:29 +0700

    add & fix style, placeholder, prefix, maxLength

--- src/components/NBTextInput.js
+++ src/components/NBTextInput.js
@@ -16,6 +16,9 @@ const styles = StyleSheet.create({
     marginLeft: 15,
     color: '#d9534e',
   },
+  labelHelper: {
+    color: "#575757"
+  }
 });
 
 const NBTextInput = ({
@@ -33,40 +36,59 @@ const NBTextInput = ({
   inlineLabel,
   floatingLabel,
   stackedLabel,
-  regular,
+  style,
+  prefix,
+  placeholder,
+  maxLength,
   meta: { touched, error },
 }) => (
-  <View style={styles.container}>
-    <Item
-      fixedLabel={fixedLabel}
-      inlineLabel={inlineLabel}
-      floatingLabel={floatingLabel}
-      stackedLabel={stackedLabel}
-      regular={regular}
-      error={!!(touched && error)}>
-      <Label>
-        {error ? `${label} *` : label}
-      </Label>
-      <Input
-        {...input}
-        value={input.value.toString()}
-        disabled={disabled || false}
-        getRef={c => (typeof (getRef) === 'function' ? getRef(c) : null)}
-        secureTextEntry={secureTextEntry}
-        keyboardType={keyboardType}
-        onSubmitEditing={onSubmit}
-        onChangeText={input.onChange}
-        autoFocus={autoFocus}
-        returnKeyType={returnKeyType}
-        autoCapitalize={autoCapitalize}
-        style={disabled ? { color: 'gray' } : null}
-      />
-    </Item>
-    <Text style={styles.formMessage} note>
-      {touched && error ? error : ''}
-    </Text>
-  </View>
-);
+    <View style={styles.container}>
+      <Text style={styles.labelHelper}>{prefix && (label)}</Text>
+      <Item
+        fixedLabel={fixedLabel}
+        inlineLabel={inlineLabel}
+        floatingLabel={floatingLabel}
+        stackedLabel={stackedLabel}
+        disabled
+        error={!!(touched && error)}>
+        <Label style={
+          prefix &&
+          { paddingBottom: 3 }
+        }>
+          {
+            prefix ?
+              prefix
+              : label ?
+                error ?
+                  `${label} *`
+                  : label
+                : null
+          }
+        </Label>
+        <Input
+          {...input}
+          value={input.value.toString()}
+          disabled={disabled || false}
+          getRef={c => (typeof (getRef) === 'function' ? getRef(c) : null)}
+          secureTextEntry={secureTextEntry}
+          keyboardType={keyboardType}
+          onSubmitEditing={onSubmit}
+          onChangeText={input.onChange}
+          autoFocus={autoFocus}
+          returnKeyType={returnKeyType}
+          autoCapitalize={autoCapitalize}
+          placeholder={placeholder}
+          maxLength={maxLength}
+          style={
+            disabled ? { color: 'grey', style } : style
+          }
+        />
+      </Item>
+      <Text style={styles.formMessage} note>
+        {touched && error ? error : ''}
+      </Text>
+    </View>
+  );
 
 NBTextInput.propTypes = {
   input: PropTypes.object.isRequired,
@@ -80,10 +102,13 @@ NBTextInput.propTypes = {
   returnKeyType: PropTypes.string,
   autoCapitalize: PropTypes.string,
   meta: PropTypes.object.isRequired,
+  placeholder: PropTypes.string,
+  prefix: PropTypes.string,
+  maxLength: PropTypes.number
 };
 
 NBTextInput.defaultProps = {
-  label: '',
+  label: null,
   secureTextEntry: false,
   keyboardType: 'default',
   onSubmit: null,
@@ -92,6 +117,7 @@ NBTextInput.defaultProps = {
   autoFocus: false,
   returnKeyType: 'done',
   autoCapitalize: 'none',
+  prefix: ''
 };
 
 export default NBTextInput;
