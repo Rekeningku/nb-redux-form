6e45e519b210e87ac852cce4d900327fbf8314ea
Dicky Indra Asmara <dickyindraofficial@gmail.com>
2018-08-12 18:00:05 +0700

    add NBPicker

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ export { default as NBTextInput } from './src/components/NBTextInput';
 export { default as NBSwitch } from './src/components/NBSwitch';
 export { default as NBTextArea } from './src/components/NBTextArea';
 export { default as NBDatePicker } from './src/components/NBDatePicker';
+export { default as NBPicker } from './src/components/NBPicker';
